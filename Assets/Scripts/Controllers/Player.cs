1f7260a564b6c640daf63ca8759e788622b138bf
tangmanp <tangmanp@OA-S141-15>
2026-09-16 10:24:32 -0400

    in class exercise : Spawn bomb at Offset (1-2) progress

---
+++ Assets/Scripts/Controllers/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public Transform enemyTransform;
+    public GameObject bombPrefab;
+    public List<Transform> asteroidTransforms;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.bKey.wasPressedThisFrame)
+        {
+            SpawnBombAtOffset(Vector3.up);
+        }
+
+    }
+
+    void SpawnBombAtOffset(Vector3 inOffset)
+    {
+        
+    }
+
+}
