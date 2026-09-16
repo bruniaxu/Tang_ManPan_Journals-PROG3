2a6be5c3b1ebbe4cddea9c3cc7afd092ee07010b
tangmanp <tangmanp@OA-S141-15>
2026-09-16 11:07:20 -0400

    in class: exe 2 : normalize as a method

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -7,7 +7,14 @@ public class Player : MonoBehaviour
     public Transform enemyTransform;
     public GameObject bombPrefab;
     public List<Transform> asteroidTransforms;
-    
+
+    void Start()
+    {
+        Debug.Log(NormalizeVector(new Vector2(3, 4)));
+        Debug.Log(NormalizeVector(new Vector2(-3, 2)));
+        Debug.Log(NormalizeVector(new Vector2(1.5f, -3.5f)));
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -21,6 +28,15 @@ public class Player : MonoBehaviour
     void SpawnBombAtOffset(Vector3 inOffset)
     {
         Instantiate(bombPrefab, transform.position + inOffset, Quaternion.identity);
+    }
+
+    Vector2 NormalizeVector(Vector2 inVector)
+    {
+        float magnitude = inVector.magnitude;
+
+        Vector2 outVector = new Vector2(inVector.x / magnitude, inVector.y / magnitude);
+
+        return outVector;
     }
 
 }
