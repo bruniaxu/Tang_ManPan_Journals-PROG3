6bc188ace0ac0dbe6d0e1abc341c7e68af1ee432
Bruni Tang <bruniaxu@gmail.com>
2026-09-22 15:19:33 -0400

    completed task 4

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -41,6 +41,11 @@ public class Player : MonoBehaviour
         {
             WarpPlayer(enemyTransform, 1f);
         }
+
+        if (Keyboard.current.aKey.wasPressedThisFrame)
+        {
+            DetectAsteroids(10f, asteroidTransforms);
+        }
     }
 
     void SpawnBombAtOffset(Vector3 inOffset)
@@ -101,5 +106,21 @@ public class Player : MonoBehaviour
     {
         transform.position = Vector3.Lerp(transform.position, target.position, ratio);
     }
-
+    void DetectAsteroids(float inMaxRange, List<Transform> inAsteroids)
+    {
+        foreach (Transform asteroid in asteroidTransforms)
+        {
+            float distance = Vector3.Distance(transform.position, asteroid.position);
+            if (distance <= inMaxRange)
+            {
+                Vector3 direction = asteroid.position - transform.position;
+                direction = direction.normalized;
+                // Make the direction 2.5 units long
+                Vector3 lineDirection = direction * 2.5f;
+                Vector3 lineEnd = transform.position + lineDirection;
+                Debug.DrawLine(transform.position, lineEnd, Color.green, 1f);
+            }
+        }
+        
+    }
 }
